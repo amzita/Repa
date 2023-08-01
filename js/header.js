ed579efc550f0bf2be2e2c1ff254d3180aa8d5b4
Alejandra <amzita@scl.edu.gt>
2023-08-01 08:39:29 -0600

    reparacion

---
+++ js/header.js
@@ -0,0 +1,14 @@
+let header_DOM = document.querySelector("#header")
+
+let header_componente = `
+    <h2>🖥️ Estudiantes Zitá</h2>
+    <ul>
+        <li>título 1</li>
+        <li>título 2</li>
+        <li>título 3</li>
+        <li>título 4</li>
+    </ul>
+`;
+
+
+header_DOM.innerHTML = header_componente;
