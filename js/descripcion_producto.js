2e43082ca422ccb7b5a6fbb03f558741159a65bf
Alejandra <amzita@scl.edu.gt>
2023-08-03 07:14:32 -0600

    Suviendo archivo

---
+++ js/descripcion_producto.js
@@ -0,0 +1,41 @@
+import { data } from "./data.js"
+
+
+  /*  let maiin  = document.querySelector("#main");
+    export let caja_descripcion = ""
+
+data.forEach((cada_item) => {
+
+    caja_descripcion= document.createElement("div")
+    caja_descripcion.className="caja_descrip";
+    caja_descripcion.innerHTML=`
+        <div class="cajaimg" >
+            <img class="Grafico" src="${cada_item.img}" alt="">
+        </div>           
+            <h2>${cada_item.titulo}</h2>
+            <p>${cada_item.descripcion}</p>
+            <span>Q. ${cada_item.precio}</span>
+       
+    `;
+    maiin.appendChild(caja_descripcion);
+
+});*/
+
+
+export let caja_descripcion = `
+
+        <p>Holaaaa</p>
+        
+           
+
+`
+
+/*********
+ * <div class="cajaimg" >
+            <img class="Grafico" src="${cada_item.img}" alt="">
+        </div>           
+            <h2>${cada_item.titulo}</h2>
+            <p>${cada_item.descripcion}</p>
+            <span>Q. ${cada_item.precio}</span> 
+ * **** */
+
--- js/header.js
+++ js/header.js
@@ -1,14 +1,17 @@
 let header_DOM = document.querySelector("#header")
 
 let header_componente = `
-    <h2>🖥️ Estudiantes Zitá</h2>
-    <ul>
-        <li>título 1</li>
-        <li>título 2</li>
-        <li>título 3</li>
-        <li>título 4</li>
-    </ul>
-`;
+        <h2> Técnico </h2>
+        <ul>
+            <li>PC</li>
+            <li>Laptop</li>
+            <li>Consola</li>
+            <li>Software</li>
+        </ul>            
+   
+`
+
+header_DOM.innerHTML= header_componente;
 
 
 header_DOM.innerHTML = header_componente;
